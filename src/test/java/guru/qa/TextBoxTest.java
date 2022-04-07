c7ae6eff87cfa081c091b588c3e1240cf8bf99be
zhanna sencenko <jannas93@mail.ru>
2022-04-07 20:18:04 +0300

    some changes for TextBox

--- src/test/java/guru/qa/TextBoxTest.java
+++ src/test/java/guru/qa/TextBoxTest.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -17,8 +18,28 @@ public class TextBoxTest {
     void fillFormTest() {
         open("/automation-practice-form");
 
-        $("[id=firstName]").setValue("Zhanna");
-        $("[id=lastName]").setValue("Senchenko");
-        $("[id=userEmail]").setValue("zhanna_senchenko@gmail.com");
+        $("#firstName").setValue("Zhanna");
+        $("#lastName").setValue("Senchenko");
+        $("#userEmail").setValue("zhanna_senchenko@gmail.com");
+
+        $("#genterWrapper").$(byText("Female")).click();
+
+        $("#userNumber").setValue("9856712322");
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("July");
+        $(".react-datepicker__year-select").selectOption("1990");
+        $(byText("15")).click();
+
+        $("#subjectsInput").setValue("History").pressEnter();
+
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+
+        $("#uploadPicture").uploadFromClasspath("cat.jpg");
+
+        $("#currentAddress-wrapper").setValue("Starovatutinskij proezd 15");
+
+
+
     }
 }
