19f8dc724077802642ddca6e82abbfe8937eada5
zhanna sencenko <jannas93@mail.ru>
2022-04-15 00:14:28 +0300

    added some methods

--- src/test/java/com/demoqa/pages/RegistrationFormPage.java
+++ src/test/java/com/demoqa/pages/RegistrationFormPage.java
@@ -1,12 +1,14 @@
 package com.demoqa.pages;
 
 import com.codeborne.selenide.SelenideElement;
+import com.demoqa.pages.components.CalendarComponent;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationFormPage {
+    CalendarComponent calendar = new CalendarComponent();
 
     // locators
     SelenideElement firstNameInput = $("#firstName");
@@ -41,10 +43,56 @@ public class RegistrationFormPage {
         return this;
     }
 
-    public RegistrationFormPage checkResult(String key, String value) {
-        $(".table-responsive").$(byText(key))
-                .parent().shouldHave(text(value));
+    public RegistrationFormPage setUserNumber(String value) {
+        $("#userNumber").$(byText(value)).click();
+        return this;
+    }
 
+    public RegistrationFormPage setDateOfBirth(String day, String month, String year) {
+        $("#dateOfBirthInput").click();
+        calendar.setDate(day, month, year);
+        return this;
+    }
+
+    public RegistrationFormPage setSubject(String value) {
+        $("#subjectsInput").setValue(value).pressEnter();
+        return this;
+    }
+
+    public RegistrationFormPage setHobby(String value) {
+        $("#hobbiesWrapper").$(byText(value)).click();
+        return this;
+    }
+
+    public RegistrationFormPage setUploadPicture(String value) {
+        $("#uploadPicture").uploadFromClasspath(value);
+        return this;
+    }
+
+    public RegistrationFormPage setCurrentAddress(String value) {
+         $("#currentAddress").setValue(value);
+         return this;
+    }
+
+    public RegistrationFormPage setState(String value) {
+        $("#state").click();
+        $(byText(value)).click();
+        return this;
+    }
+
+    public RegistrationFormPage setCity(String value) {
+        $("#city").click();
+        $(byText(value)).click();
+        return this;
+    }
+
+    public void clickSubmit() {
+        $("#submit").click();
+    }
+
+    //check
+    public RegistrationFormPage checkResult(String value) {
+        $(".modal-body").shouldHave(text(value));
         return this;
     }
 }
